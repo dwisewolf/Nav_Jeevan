674310419564d43c87a9d9e2bd9c41e5211d7a9e
Joel Joys <joel.welezo@gmail.com>
2020-04-06 23:11:24 +0530

    host added

--- Student/settings.py
+++ Student/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gh#ozef_ywda&*@zok++^^$$%)42mobn7i*=^!vgv4l&x_$rh!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['139.59.23.131']
 
 
 # Application definition
